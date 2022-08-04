84465a8e619db69a05dd156975bd15c14e6a13ff
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-08-04 12:14:29 -0500

    It is created data class Language

---
+++ app/src/main/java/com/example/applanguagepgm/Language.kt
@@ -0,0 +1,11 @@
+package com.example.applanguagepgm
+
+data class Language(
+    var imgIconLanguage: String,
+    var year:String,
+    var paradigma:String,
+    var useLgn:String,
+    var createBy:String,
+    var infCreator:String,
+    var description:String,
+    var favorite:Boolean)
