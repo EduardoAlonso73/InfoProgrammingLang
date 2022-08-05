332239699f9ba915c258c75b8c29bc8e52b1ecd1
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-08-05 16:05:37 -0500

    We create the class Dao  where we define the Querys [SELECT,INSERT,UPDATE,DELETE]

--- app/src/main/java/com/example/applanguagepgm/LanguageDao.kt
+++ app/src/main/java/com/example/applanguagepgm/LanguageDao.kt
@@ -1,4 +1,23 @@
 package com.example.applanguagepgm
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
 interface LanguageDao {
+    @Query("SELECT * FROM languageadapter")
+    fun getListLanguage():MutableList<LanguageEntity>
+    @Insert
+    fun addLanguage(languageEntity: LanguageEntity)
+    // ******* FUNCTION FOR UPDATE STORES *******
+    @Update
+    fun updateLanguage(languageEntity: LanguageEntity)
+
+    // ******* FUNCTION FOR DELETE STORES *******
+    @Delete
+    fun deleteLanguage(languageEntity: LanguageEntity)
+
+
+
 }
