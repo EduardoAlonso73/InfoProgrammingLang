b8bc8b8dfc79832a69125576f286adfc9aff738f
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-08-05 14:13:58 -0500

    We configure the RecyclerView in the initRecyclerView function

--- app/src/main/java/com/example/applanguagepgm/MainActivity.kt
+++ app/src/main/java/com/example/applanguagepgm/MainActivity.kt
@@ -2,14 +2,36 @@ package com.example.applanguagepgm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.applanguagepgm.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var mainBinding: ActivityMainBinding
+class MainActivity : AppCompatActivity(),OnClickListener {
+    private lateinit var mBinding: ActivityMainBinding
+    private  lateinit var  mAdapter:LanguageAdapter
+    private lateinit var  mGridLayoutManager: GridLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        mainBinding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(mainBinding.root)
+        mBinding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+
+        mBinding.btnSave.setOnClickListener {
+            val listLanguage= Language(nameLanguage=mBinding.etName.text.toString().toString())
+            mAdapter.addLanguage(listLanguage)
+        }
+
+        initRecyclerView()
     }
+
+    private fun initRecyclerView() {
+        mAdapter=LanguageAdapter(mutableListOf(),this)
+        mGridLayoutManager=GridLayoutManager(this,1)
+        mBinding.recyclerView.apply{
+            setHasFixedSize(true)
+            layoutManager=mGridLayoutManager
+            adapter=mAdapter
+        }
+    }
+    
 }
