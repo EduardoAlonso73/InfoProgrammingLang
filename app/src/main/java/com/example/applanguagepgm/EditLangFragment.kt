11e9d4b4935e6bc3a60266fb378e957ff7f643ea
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-08-09 15:13:24 -0500

    Processes of adding in the database

--- app/src/main/java/com/example/applanguagepgm/EditLangFragment.kt
+++ app/src/main/java/com/example/applanguagepgm/EditLangFragment.kt
@@ -5,29 +5,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.applanguagepgm.databinding.FragmentEditLangBinding
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import kotlin.concurrent.thread
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [EditLang.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EditLangFragment : Fragment() {
+    private  lateinit var mBinding:FragmentEditLangBinding
+    private var mActivity:MainActivity?=null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View {
+        mBinding= FragmentEditLangBinding.inflate(inflater,container,false)
+        return mBinding.root
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_edit_lang, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mActivity = activity as? MainActivity
+        mActivity?.supportActionBar?.hide()
+
+        with(mBinding){
+            btnCancel.setOnClickListener { mActivity?.onBackPressed() }
+            btnSave.setOnClickListener { addLang() }
+        }
     }
 
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mActivity?.supportActionBar?.show()
+    }
+
+    /*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+            ----------- Method for BD ------------
+    -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
+
+    private fun addLang() {
+        val lang=LanguageEntity(
+          nameLanguage = mBinding.etNameLang.text.toString().trim(),
+          year = mBinding.etYear.text.toString().trim(),
+          useLgn = mBinding.etUsedLang.text.toString().trim(),
+          imgIconLanguage = mBinding.etUrlIconLang.text.toString().trim(),
+          fotoCreateBy = mBinding.etlUrlImgCreateBy.text.toString().trim(),
+          infCreator = mBinding.etiInfCreateBy.text.toString().trim(),
+          description = mBinding.etiInfLanguage.text.toString().trim()
+        );
+        doAsync {
+            lang.id=LanguageApplication.database.languageDao().addLanguage(lang)
+            uiThread {
+                mActivity?.addLanguage(lang)
+                mActivity?.onBackPressed()
+        }
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/applanguagepgm/MainAux.kt
@@ -0,0 +1,4 @@
+package com.example.applanguagepgm
+
+interface MainAux {
+}
