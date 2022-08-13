0704e228f70a747f250a7ee59e3af55901c4885a
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-08-13 08:15:18 -0500

    We change the intialization of LangEntity parameters

--- app/src/main/java/com/example/applanguagepgm/LanguageEntity.kt
+++ app/src/main/java/com/example/applanguagepgm/LanguageEntity.kt
@@ -3,18 +3,34 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 
-@Entity(tableName = "LanguageAdapter")
+@Entity(tableName = "LanguageEntity")
 data class LanguageEntity(
     @PrimaryKey(autoGenerate=true) var id: Long = 0,
-    var imgIconLanguage: String = "",
+    var imgIconLanguage: String ,
     var nameLanguage: String,
-    var year: String = "",
+    var year: String ,
     var fotoCreateBy: String = "",
     var useLgn: String = "",
-    var createBy: String = "",
+    var createBy: String ,
     var infCreator: String = "",
-    var description: String = "",
+    var description: String ,
     var isFavorite: Boolean = false
-)
+
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as LanguageEntity
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
 
 
