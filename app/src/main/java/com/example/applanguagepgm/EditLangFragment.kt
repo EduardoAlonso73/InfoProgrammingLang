aba3e8efcd99c75f1b34d75b064d9a4d19a5ba7b
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-08-13 08:00:37 -0500

    Update processes langEntity

--- app/src/main/java/com/example/applanguagepgm/EditLangFragment.kt
+++ app/src/main/java/com/example/applanguagepgm/EditLangFragment.kt
@@ -22,6 +22,8 @@ import kotlin.concurrent.thread
 class EditLangFragment : Fragment() {
     private  lateinit var mBinding:FragmentEditLangBinding
     private var mActivity:MainActivity?=null
+    private  var isEdit:Boolean=false
+    private  var mLangEntity:LanguageEntity?=null
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -34,16 +36,49 @@ class EditLangFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         mActivity = activity as? MainActivity
         mActivity?.supportActionBar?.hide()
+        val id=arguments?.getLong(getString(R.string.arg_id),0)
 
+         if(id!=null && id!=0L){
+             isEdit=true
+             getDataLang(id)
+         }else{
+             isEdit=false
+             mLangEntity=LanguageEntity(nameLanguage ="",year = "", useLgn = "",createBy = "", imgIconLanguage ="",fotoCreateBy = "",
+                 infCreator = "",
+                 description =""
+             );
+
+         }
         with(mBinding){
-            btnCancel.setOnClickListener { mActivity?.onBackPressed()
-            //    hideKeyboard()
-            }
-
+            btnCancel.setOnClickListener { mActivity?.onBackPressed()/*hideKeyboard()*/ }
             btnSave.setOnClickListener { addLang() }
             etUrlIconLang.addTextChangedListener {showPreviewImage(mBinding.etUrlIconLang.text.toString(), mBinding.imglUrlIconLang) }
             etlUrlImgCreateBy.addTextChangedListener {showPreviewImage(mBinding.etlUrlImgCreateBy.text.toString(),mBinding.imgUrlImgCreateBy)}
         }
+    }
+
+    private fun getDataLang(id: Long) {
+        doAsync {
+            mLangEntity=LanguageApplication.database.languageDao().getDtaLangById(id)
+            uiThread {
+                mLangEntity.let{setDtLangUI(mLangEntity!!) }
+            }
+        }
+
+    }
+
+    private fun setDtLangUI(mLangEntity: LanguageEntity){
+        with(mBinding){
+            etNameLang.setText(mLangEntity.nameLanguage)
+            etYear.setText(mLangEntity.year)
+            etUsedLang.setText(mLangEntity.useLgn)
+            etCreateBy.setText(mLangEntity.createBy)
+            etUrlIconLang.setText(mLangEntity.imgIconLanguage)
+            etlUrlImgCreateBy.setText(mLangEntity.fotoCreateBy)
+            etiInfCreateBy.setText(mLangEntity.infCreator)
+            etiInfLanguage.setText(mLangEntity.description)
+        }
+
     }
 
 
@@ -76,20 +111,31 @@ class EditLangFragment : Fragment() {
     -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
 
     private fun addLang() {
-        val lang=LanguageEntity(
-          nameLanguage = mBinding.etNameLang.text.toString().trim(),
-          year = mBinding.etYear.text.toString().trim(),
-          useLgn = mBinding.etUsedLang.text.toString().trim(),
-          imgIconLanguage = mBinding.etUrlIconLang.text.toString().trim(),
-          fotoCreateBy = mBinding.etlUrlImgCreateBy.text.toString().trim(),
-          infCreator = mBinding.etiInfCreateBy.text.toString().trim(),
-          description = mBinding.etiInfLanguage.text.toString().trim()
-        );
+           with(mLangEntity!!) {
+            nameLanguage = mBinding.etNameLang.text.toString().trim()
+            year = mBinding.etYear.text.toString().trim()
+            useLgn = mBinding.etUsedLang.text.toString().trim()
+            createBy = mBinding.etCreateBy.text.toString().trim()
+            imgIconLanguage = mBinding.etUrlIconLang.text.toString().trim()
+            fotoCreateBy = mBinding.etlUrlImgCreateBy.text.toString().trim()
+            infCreator = mBinding.etiInfCreateBy.text.toString().trim()
+            description = mBinding.etiInfLanguage.text.toString().trim()
+        }
+
         doAsync {
-            lang.id=LanguageApplication.database.languageDao().addLanguage(lang)
+            if (isEdit)LanguageApplication.database.languageDao().updateLanguage(mLangEntity!!)
+            else mLangEntity!!.id=LanguageApplication.database.languageDao().addLanguage(mLangEntity!!)
             uiThread {
                 hideKeyboard()
-                mActivity?.addLanguage(lang)
+
+                if(isEdit) {
+                    Toast.makeText(context, "Update", Toast.LENGTH_SHORT).show()
+                    mActivity?.updateStore(mLangEntity!!)
+                }
+                else {
+                    Toast.makeText(context, "Add", Toast.LENGTH_SHORT).show()
+                    mActivity?.addLanguage(mLangEntity!!)
+                }
                 mActivity?.onBackPressed()
         }
         }
