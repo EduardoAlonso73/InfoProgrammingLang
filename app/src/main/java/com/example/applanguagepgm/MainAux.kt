f810c552e5d616abd238828170cb089e7443cad3
EduardoAlonso73 <eduardo.alonso.73@gmail.com>
2022-08-20 16:05:53 -0500

    We create the startIntent function

--- app/src/main/java/com/example/applanguagepgm/MainAux.kt
+++ app/src/main/java/com/example/applanguagepgm/MainAux.kt
@@ -1,6 +1,9 @@
 package com.example.applanguagepgm
 
+import android.content.Intent
+
 interface MainAux {
     fun addLanguage(languageEntity: LanguageEntity)
     fun updateLang(languageEntity: LanguageEntity)
+    fun startIntent(intent: Intent)
 }
